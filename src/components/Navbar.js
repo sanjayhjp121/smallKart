2e36d8318b972e045e86f19b9d2b4060e46d26f6
Sanjay Kumar <72185623+sanjayhjp121@users.noreply.github.com>
2022-06-09 21:34:37 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Small Kart
+            </a>
+            
+            
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Navbar;
